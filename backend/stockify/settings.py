4fc504c90538e997efc7aff2c6c9125a8e7d9d99
BhaveshC2003 <bhaveshpc30@gmail.com>
2023-11-20 00:05:06 +0530

    Changes in requirements.txt

--- backend/stockify/settings.py
+++ backend/stockify/settings.py
@@ -35,14 +35,18 @@ ALLOWED_HOSTS = '*'
 #     'http://localhost:3000',
 #     'http://127.0.0.1:3000',    #Added this
 # )
+
+CORS_ALLOW_CREDENTIALS = True   #Added this
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
 ]
 
 CORS_ORIGIN_WHITELIST = ( 'localhost:3000', '127.0.0.1:3000')
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000", "http://localhost:3000"]
+SESSION_COOKIE_SAMESITE = None
+CSRF_COOKIE_SECURE = True
 
-CORS_ALLOW_CREDENTIALS = True   #Added this
 
 # Application definition
 
